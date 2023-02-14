79914b93a49e924b4f83d44f13c0d114ba49aedf
Joseph (Ting-Chou) Lin <tlin@linkedin.com>
2023-02-14 13:53:56 -0800

    Add zstd-jni licnese to Native.java
    The file is modified from `[luben/zstd-jni](https://github.com/luben/zstd-jni/blob/944df7a3ce4950ef800030810d3dc9488a397b4e/src/main/java/com/github/luben/zstd/util/Native.java)`.
    Since it's under BSD license, appropriate license disclaimer should be added.

--- src/main/java/com/linkedin/ktls/util/Native.java
+++ src/main/java/com/linkedin/ktls/util/Native.java
@@ -1,3 +1,36 @@
+/*
+ * This source file is modified from Luben's zstd-jni
+ * https://github.com/luben/zstd-jni/blob/944df7a3ce4950ef800030810d3dc9488a397b4e/src/main/java/com/github/luben/zstd/util/Native.java
+ *
+ * And the below is the original license from the snapshot
+ * ================================================================================
+ * Zstd-jni: JNI bindings to Zstd Library
+ *
+ * Copyright (c) 2015-present, Luben Karavelov/ All rights reserved.
+ *
+ * BSD License
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice, this
+ *   list of conditions and the following disclaimer in the documentation and/or
+ *   other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package com.linkedin.ktls.util;
 
 import java.io.File;
