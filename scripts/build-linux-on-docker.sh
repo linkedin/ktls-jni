0ad503acd2e666eda815a693a10092dca1647127
Vishnu Praneeth Sabbavarapu <vpraneeth@linkedin.com>
2023-06-09 16:08:07 -0700

    Resolve PR comments to fix newline in build script

--- scripts/build-linux-on-docker.sh
+++ scripts/build-linux-on-docker.sh
@@ -1,7 +1,6 @@
 SCRIPT_PATH=$(realpath "$0")
 SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
 
-
 PROJ_DIR=$(dirname "$SCRIPT_DIR")
 
 SRC_PATH_CONTAINER=/tmp/ktls-jni
@@ -11,3 +10,4 @@ rm -rf build
 
 echo "Starting build-native.sh ..."
 docker run -it -v ${PROJ_DIR}:${SRC_PATH_CONTAINER} --user $(id -u):$(id -g)  ktls-jni-build ${SRC_PATH_CONTAINER}/scripts/build-native.sh
+
