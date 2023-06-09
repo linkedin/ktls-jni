75f13748d7e6ce39d2951e8575496fa5c1ddd418
Vishnu Praneeth Sabbavarapu <vpraneeth@linkedin.com>
2023-06-09 15:37:59 -0700

    Resolve comments for build and image script

--- scripts/build-linux-on-docker.sh
+++ scripts/build-linux-on-docker.sh
@@ -1,29 +1,13 @@
-
 SCRIPT_PATH=$(realpath "$0")
 SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
 
-# Traverse upwards until the project directory is found
-while [ ! -d "$SCRIPT_DIR/ktls-jni" ] && [ "$SCRIPT_DIR" != "/" ]; do
-    SCRIPT_DIR=$(dirname "$SCRIPT_DIR")
-done
 
-# Check if the project directory is found
-if [ -d "$SCRIPT_DIR/ktls-jni" ]; then
-    SCRIPT_DIR=$SCRIPT_DIR/ktls-jni
-else
-    echo "Project directory not found."
-fi
+PROJ_DIR=$(dirname "$SCRIPT_DIR")
 
-echo $SCRIPT_DIR
 SRC_PATH_CONTAINER=/tmp/ktls-jni
 
 echo "Removing build folder ..."
 rm -rf build
 
 echo "Starting build-native.sh ..."
-docker run -it -v ${SCRIPT_DIR}:${SRC_PATH_CONTAINER} --user $(id -u):$(id -g)  ktls-jni-build ${SRC_PATH_CONTAINER}/scripts/build-native.sh
-
-
-
-
-
+docker run -it -v ${PROJ_DIR}:${SRC_PATH_CONTAINER} --user $(id -u):$(id -g)  ktls-jni-build ${SRC_PATH_CONTAINER}/scripts/build-native.sh
--- scripts/prepare-docker-image.sh
+++ scripts/prepare-docker-image.sh
@@ -1,17 +1,6 @@
 SCRIPT_PATH=$(realpath "$0")
 SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
 
-# Traverse upwards until the project directory is found
-while [ ! -d "$SCRIPT_DIR/ktls-jni" ] && [ "$SCRIPT_DIR" != "/" ]; do
-    SCRIPT_DIR=$(dirname "$SCRIPT_DIR")
-done
+PROJ_DIR=$(dirname "$SCRIPT_DIR")
 
-# Check if the project directory is found
-if [ -d "$SCRIPT_DIR/ktls-jni" ]; then
-    SCRIPT_DIR=$SCRIPT_DIR/ktls-jni
-else
-    echo "Project directory not found."
-fi
-
-
-docker build -t ktls-jni-build -f "$SCRIPT_DIR/Dockerfile" "$SCRIPT_DIR"
+docker build -t ktls-jni-build -f "$PROJ_DIR/Dockerfile" "$SCRIPT_DIR"
