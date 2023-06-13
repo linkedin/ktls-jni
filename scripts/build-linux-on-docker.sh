ece2351237651e5110a95c88b2b6e67b19c582e1
Vishnu Praneeth Sabbavarapu <vpraneeth@linkedin.com>
2023-06-12 19:44:38 -0700

    Add changes for resolving PR comments and fix for mounting problem

--- scripts/build-linux-on-docker.sh
+++ scripts/build-linux-on-docker.sh
@@ -5,10 +5,5 @@ PROJ_DIR=$(dirname "$SCRIPT_DIR")
 
 SRC_PATH_CONTAINER=/tmp/ktls-jni
 
-echo "Removing build folder ..."
-#rm -rf build
-
-echo "Starting build-native.sh ..."
-docker run  --user $(id -u):$(id -g) -v `pwd`:/tmp/ktls-jni -w /tmp/ktls-jni ktls-jni-build ${SRC_PATH_CONTAINER}/scripts/build-native.sh
-
-#docker run --user $(id -u):$(id -g) -v `pwd`:/tmp/ktls-jni ktls-jni-build
+echo "Running the native build inside docker ..."
+docker run  --user $(id -u):$(id -g) -v $PROJ_DIR:$SRC_PATH_CONTAINER ktls-jni-build ${SRC_PATH_CONTAINER}/scripts/build-native.sh
